8657803345e02dfcfd115489b09318f6ace74bab
Gurjit <gurjiitsingh@gmail.com>
2026-09-10 16:40:47 +0530

    update

--- src/app/pos/checkout/page.tsx
+++
@@ -1,9 +0,0 @@
-import POSCheckout from "@/components/pos/POSCheckout";
-
-export default function Page() {
-  return (
-    <div className="max-w-5xl mx-auto py-10 px-4">
-      <POSCheckout />
-    </div>
-  );
-}
