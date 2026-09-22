7223911af29109d25bf8fa3f431deff5e6f19abe
Gurjit <gurjiitsingh@gmail.com>
2026-09-22 12:10:11 +0530

    update

--- src/components/MiniCart/SideCart.js
+++ src/components/MiniCart/SideCart.js
@@ -72,7 +72,7 @@ export const SideCart = () => {
                 aria-label="Sidebar"
               >
                 {/* Header */}
-                <div className="flex items-center justify-between px-4 py-3 border-b border-gray-200 bg-gray-50">
+                <div className="flex items-center justify-between px-4 pt-23 pb-3 border-b border-gray-200 bg-gray-50">
                   <span className="text-lg font-semibold text-gray-800">
                     {TEXT.cart_sidebar_title}
                   </span>
